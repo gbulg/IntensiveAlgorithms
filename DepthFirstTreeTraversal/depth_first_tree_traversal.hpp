1187af1c1210cf914bbc4b96c99425095125b66c
Georgy Bulgakov <georgybulgakov@Georgys-MacBook-Air.local>
2023-08-14 20:21:32 +0300

    dfs

--- DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
+++ DepthFirstTreeTraversal/depth_first_tree_traversal.hpp
@@ -15,5 +15,39 @@ struct TreeNode {
 
 class Solution {
 public:
-  std::vector<int> distanceK(TreeNode *root, TreeNode *target, int k) {}
+    void FillNeighbours(TreeNode* root, std::unordered_map<TreeNode *, std::vector<TreeNode *>>& neighbours) {
+        if (root->left) {
+            neighbours[root].push_back(root->left);
+            neighbours[root->left].push_back(root);
+            FillNeighbours(root->left, neighbours);
+        }
+        if (root->right) {
+            neighbours[root].push_back(root->right);
+            neighbours[root->right].push_back(root);
+            FillNeighbours(root->right, neighbours);
+        }
+    }
+
+    void dfs(TreeNode* node, int dist, std::vector<int>& result, std::unordered_map<TreeNode *, int>& visited, std::unordered_map<TreeNode *, std::vector<TreeNode *>>& neighbours) {
+        visited[node] = 1;
+        if (dist == 0) {
+            result.push_back(node->val);
+            return;
+        }
+        for (auto neighbour : neighbours[node]) {
+            if (visited[neighbour]) continue;
+            dfs(neighbour, dist - 1, result, visited, neighbours);
+        }
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        std::unordered_map<TreeNode *, std::vector<TreeNode *>> neighbours;
+        FillNeighbours(root, neighbours);
+        
+        std::unordered_map<TreeNode *, int> visited;
+        std::vector<int> result;
+        dfs(target, k, result, visited, neighbours);
+
+        return result;
+    }
 };
