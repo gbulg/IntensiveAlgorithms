77cecbebb961d12aaaf5f52141e93352afa504bd
Georgy Bulgakov <georgybulgakov@Georgys-MacBook-Air.local>
2023-08-14 21:00:28 +0300

    bfs

--- BreadthFirstTreeTraversal/breadth_first_tree_traversal.hpp
+++ BreadthFirstTreeTraversal/breadth_first_tree_traversal.hpp
@@ -17,5 +17,26 @@ struct TreeNode {
 
 class Solution {
 public:
-  std::vector<std::vector<int>> levelOrder(TreeNode *root) {}
+  std::vector<std::vector<int>> levelOrder(TreeNode *root) {
+    std::vector<std::vector<int>> result;
+
+    std::vector<int> cur_level;
+    std::queue<TreeNode *> q;
+    if (root) q.push(root);
+
+    while(!q.empty()) {
+        int level_size = q.size();
+        for (int i = 0; i < level_size; ++i) {
+            TreeNode *cur = q.front();
+            q.pop();
+
+            cur_level.push_back(cur->val);
+            if (cur->left) q.push(cur->left);
+            if (cur->right) q.push(cur->right);
+        }
+        result.push_back(cur_level);
+        cur_level.clear();
+    }
+    return result;
+  }
 };
