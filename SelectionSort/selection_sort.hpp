469cbd1f6cab4e65b667b172dafc91bbee625e31
Georgy Bulgakov <georgybulgakov@Georgys-MacBook-Air.local>
2023-08-14 18:41:50 +0300

    selection sort

--- SelectionSort/selection_sort.hpp
+++ SelectionSort/selection_sort.hpp
@@ -5,5 +5,14 @@
 // https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+  std::vector<int> sortArray(std::vector<int> &nums) {
+    for (int i = 0; i < nums.size() - 1; ++i) {
+      int min_pos = i;
+      for (int j = i; j < nums.size(); ++j) {
+        if (nums[j] < nums[min_pos]) min_pos = j;
+      }
+      std::swap(nums[i], nums[min_pos]);
+    }
+    return nums;
+  }
 };
