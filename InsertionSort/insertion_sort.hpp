9f52ee8ec2f9e28a5cf79a7b0cb43f0427b3bd7b
Georgy Bulgakov <georgybulgakov@Georgys-MacBook-Air.local>
2023-08-14 19:28:17 +0300

    insertion sort

--- InsertionSort/insertion_sort.hpp
+++ InsertionSort/insertion_sort.hpp
@@ -13,5 +13,45 @@ struct ListNode {
 
 class Solution {
 public:
-  ListNode *insertionSortList(ListNode *head) {}
+  ListNode *insertionSortList(ListNode *head) {
+    ListNode *cur = head;
+
+    std::vector<int> nums;
+    while (cur) {
+      nums.push_back(cur->val);
+      cur = cur->next;
+    }
+
+    for (int i = 1; i < nums.size(); ++i) {
+      int j = i;
+      while (j > 0 && nums[j] < nums[j - 1]) {
+        std::swap(nums[j], nums[j - 1]);
+        j--;
+      }
+    }
+
+    cur = head;
+    for (int i = 0; i < nums.size(); ++i) {
+      cur->val = nums[i];
+      cur = cur->next;
+    }
+    return head;
+
+    /*
+    ListNode *cur = head;
+    while (cur) {
+      ListNode *next = cur->next;
+
+      ListNode *insert = head;
+      while (insert->next->val < cur->val) insert = insert->next;
+
+      cur->next = insert->next;
+      insert->next = cur;
+
+      cur = next;
+    }
+
+    return head;
+    */
+  }
 };
