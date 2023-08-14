559aa451c446910bedf0298ee83d2069b47ba34e
Georgy Bulgakov <georgybulgakov@Georgys-MacBook-Air.local>
2023-08-14 18:42:59 +0300

    bubble sort

--- BubbleSort/bubble_sort.hpp
+++ BubbleSort/bubble_sort.hpp
@@ -5,5 +5,11 @@
 // https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  std::vector<int> sortArray(std::vector<int> &nums) {}
+  std::vector<int> sortArray(std::vector<int> &nums) {
+    for (int i = 0; i < nums.size(); ++i) {
+      for (int j = 0; j < nums.size() - 1; ++j) {
+        if (nums[j] > nums[j + 1]) std::swap(nums[j], nums[j + 1]);
+      }
+    }
+  }
 };
